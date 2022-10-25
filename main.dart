13cc1284c3dca139a5539130d5703a290f70b53a
Gladwin Del Rosario <gladelrosario12@gmail.com>
2022-10-25 20:51:20 +0800

    updated some code

--- main.dart
+++ main.dart
@@ -2,6 +2,5 @@ import 'WinBanks.dart';
 
 void main() {
   // Running the WinBanks Console Application
-  var winBanks = new WinBanks();
-  winBanks.bankProcessFunction();
+  new WinBanks().bankProcessFunction();
 }
